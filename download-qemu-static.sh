5a13953e5747f73fdf8bb6cd150dc94a172a459a
Marcel Bargull <marcel.bargull@udo.edu>
2023-12-15 23:46:43 +0100

    Try qemu-user-static=8.1 again
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>

--- download-qemu-static.sh
+++ download-qemu-static.sh
@@ -14,15 +14,15 @@ DEBIAN_FRONTEND=noninteractive \
     apt-get install --yes --no-install-recommends \
     ca-certificates curl libarchive-tools
 
-version='8.0.3'
-build='4.fc40'
+version='8.1.3'
+build='1.fc39'
 for arch in aarch64 ppc64le s390x; do
     curl -sL \
         "https://kojipkgs.fedoraproject.org/packages/qemu/${version}/${build}/x86_64/qemu-user-static-${arch/ppc64le/ppc}-${version}-${build}.x86_64.rpm" |
         bsdtar -xf- --strip-components=3 ./usr/bin/qemu-${arch}-static
 done
 sha256sum --check << 'EOF'
-d02f52c9135453a453a80e5746eee13bb4ea9cd31080d9ad9a75e6442f930402  qemu-aarch64-static
-b68bdd489120afe8d4f76dc040651dbb0d6012d33395920fe4a1e4afb84f5b72  qemu-ppc64le-static
-68e711c57b7e74306bafc08a0884f34dde45b8866a07d4fe5506f673d9ea485b  qemu-s390x-static
+3b78154b2aa59f886b530826b5dd01be03ce3c4b29751a814d724650bcc526b2  qemu-aarch64-static
+15593a3ede391e218a61f7e5c28b714c9bce5cd2736f12127847cef1d8f0edd9  qemu-ppc64le-static
+92dd7d9824c9c41635cf91761d8f9799f3b727918ae0f645a43c4317bab5d98f  qemu-s390x-static
 EOF
